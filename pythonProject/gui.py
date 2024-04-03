b8259566c2ad3bf420c5312ecf35f473862f6da1
dhwani <shahdhwani045@gmail.com>
2024-04-04 00:41:27 +0530

    Added a new GUI feature of implementing edit button to add todo to todo.txt file

--- pythonProject/gui.py
+++ pythonProject/gui.py
@@ -4,22 +4,37 @@ import PySimpleGUI as sg
 label = sg.Text("Type in a to-do")
 input_box = sg.InputText(tooltip="Enter a to-do", key="todo")
 add_button = sg.Button("Add")
-
+list_todo = sg.Listbox(functions.get_todos(), key="todolist", size=(45, 10))
+edit_button = sg.Button("Edit")
 
 window = sg.Window('My To-Do App',
-                       layout=[[label], [input_box, add_button]],
+                       layout=[[label], [input_box, add_button], [list_todo, edit_button]],
                        font=('Helvetica', 20))
 
 while True:
     event, value = window.read()
     print(event)
     print(value)
+    print(value['todolist'])
     match event:
         case "Add":
             todos = functions.get_todos()
             new_todo = value['todo'] + "\n"
             todos.append(new_todo)
             functions.write_todos(todos)
+
+        case "Edit":
+            todo_to_edit = value['todolist'][0]
+            new_todo = value['todo']
+
+            todos = functions.get_todos()
+            index_todo = todos.index(todo_to_edit)
+            todos[index_todo] = new_todo + "\n"
+            functions.write_todos(todos)
+            window['todolist'].update(values=todos)
+
+
+
         case sg.WIN_CLOSED:
             break
 
