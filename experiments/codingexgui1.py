d97376338b5ac5a552cb64d1c6f59c3e2d7b82d5
dhwani <shahdhwani045@gmail.com>
2024-04-09 20:02:58 +0530

    Solved Coding Exercise in Feet and Inches GUI program by adding exit button and applying theme

--- experiments/codingexgui1.py
+++ experiments/codingexgui1.py
@@ -1,7 +1,7 @@
 import PySimpleGUI as sg
 from converters import convert
 
-
+sg.theme("Black")
 label_feet = sg.Text("Enter Feet: ")
 input_feet = sg.Input(key="feet")
 
@@ -9,19 +9,24 @@ label_inches = sg.Text("Enter Inches: ")
 input_inches = sg.Input(key="inches")
 
 convert_button = sg.Button("Convert")
-output_label = sg.Text(key="output", text_color="Black")
+output_label = sg.Text(key="output", text_color="White")
+exit_button = sg.Button("Exit")
 
 window = sg.Window("Convertor", [[label_feet, input_feet],
                                  [label_inches, input_inches],
-                                 [convert_button, output_label]])
+                                 [convert_button, exit_button, output_label]])
 
 while True:
     event, values = window.read()
     print(event, values)
-    feet = float(values["feet"])
-    inches = float(values["inches"])
-    meter = convert(feet, inches)
-    window["output"].update(value=f"{meter}m")
+    match event:
+        case "Convert":
+            feet = float(values["feet"])
+            inches = float(values["inches"])
+            meter = convert(feet, inches)
+            window["output"].update(value=f"{meter}m")
+        case "Exit":
+            break
 
 
 
