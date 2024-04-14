113ef6c266878f0a43a48de2813e0fd8e453102b
dhwani <shahdhwani045@gmail.com>
2024-04-14 18:59:29 +0530

    Created a web app for to-do list

---
+++ pythonProject/web.py
@@ -0,0 +1,22 @@
+import streamlit as st
+import functions
+
+def add_todo():
+    todo = st.session_state["new_todo"] + "\n"
+    todos.append(todo)
+    functions.write_todos(todos)
+
+todos = functions.get_todos()
+
+st.title("My To-Do App")
+st.subheader("This is my todo app.")
+st.write("This app is to increase your productivity.")
+
+for todo in todos:
+    st.checkbox(todo)
+
+st.text_input(label="Enter a to-do", label_visibility="hidden",
+              placeholder="Add new todo...",
+              on_change=add_todo, key="new_todo")
+
+
