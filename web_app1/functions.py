6538bd1cea204fb1ff972234e55e9026171623ce
dhwani <shahdhwani045@gmail.com>
2024-04-14 19:28:15 +0530

    created new project and copied required files in it for web-app

---
+++ web_app1/functions.py
@@ -0,0 +1,16 @@
+FILEPATH = "todos.txt"
+def get_todos(filepath=FILEPATH):
+    """ Read the text file and get the list of to-do items. """
+    with open(filepath, 'r') as file_local:
+        todos_local = file_local.readlines()
+    return todos_local
+
+
+def write_todos(todos_arg,filepath=FILEPATH):
+    """ Write the to-do items in the text file. """
+    with open(filepath, 'w') as file:
+        file.writelines(todos_arg)
+
+if __name__ == "__main__":
+    print("hello")
+    print(get_todos())
